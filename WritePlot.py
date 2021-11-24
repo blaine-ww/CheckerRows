5ffbfe71b3c9f00b02f8c4551fbc55d4ea871542
blaine-ww <43366878+blaine-ww@users.noreply.github.com>
2021-11-24 00:05:44 -0800

    Clean spacing

--- WritePlot.py
+++ WritePlot.py
@@ -21,6 +21,7 @@ import output
 exec(open('output\output.py').read()) #read output lists from output.py
 
 
+
 def writePlot(example, name=""):
     
     plt.title(name + " Visualization")
@@ -28,16 +29,12 @@ def writePlot(example, name=""):
     plt.ylabel("Y coordinates")
     plt.plot(example[0][:],example[1][:],'-o')
     
-
-    
     # only save img file if name provided
     if name:
         plt.savefig("output/"+ name + ".jpg")
     
     plt.show()
 
-    
-    
 
 
 # adjust ./output_name based on object names in main.cpp and output.py
